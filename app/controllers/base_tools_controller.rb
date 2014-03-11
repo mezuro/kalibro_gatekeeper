ae08d73bb4db43191ab0af2c2a0ae43169491904
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-03-11 11:19:53 -0300

    Get BaseTool error handling

--- app/controllers/base_tools_controller.rb
+++ app/controllers/base_tools_controller.rb
@@ -9,10 +9,18 @@ class BaseToolsController < ApplicationController
   end
 
   def get
-    base_tool = KalibroGem::Entities::BaseTool.find_by_name(params[:name]).to_hash
+    begin
+      base_tool = KalibroGem::Entities::BaseTool.find_by_name(params[:name])
+    rescue KalibroGem::Errors::RecordNotFound
+      base_tool = {error: 'RecordNotFound'}
+    end
 
     respond_to do |format|
-      format.json { render json: base_tool }
+      if base_tool.is_a?(KalibroGem::Entities::BaseTool)
+        format.json { render json: base_tool.to_hash }
+      else
+        format.json { render json: base_tool, status: :unprocessable_entity }
+      end
     end
   end
 end
