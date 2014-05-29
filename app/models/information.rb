29bf79e5f180bdaf71e5bfeb71b7659520091afc
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-05-29 11:05:55 -0300

    Information updated

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,7 +1,7 @@
 class Information
-  VERSION="0.0.1.alpha"
+  VERSION="0.0.1"
   LICENSE="AGPLv3"
-  REPOSITORY_URL="https://github.com/mezuro/"
+  REPOSITORY_URL="https://github.com/mezuro/kalibro_gatekeeper"
 
   def self.data
     {version: VERSION, license: LICENSE, repository_url: REPOSITORY_URL}
